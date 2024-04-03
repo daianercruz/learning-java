0d2f60d039809e32506b86310f6b88cfae0b3e11
Daiane Cruz <daianercruz12@gmail.com>
2024-04-03 12:27:09 -0300

    Exercicios para serem visualizados no terminal

--- Operadores/Ternario.java
+++ Operadores/Ternario.java
@@ -1,6 +1,6 @@
 public class Ternario {
    
-   public static void main (String [] args){
+   /*public static void main (String [] args){
     int a, b;
 
       a = 5;
@@ -21,5 +21,13 @@ public class Ternario {
 
             System.out.println(resultado);
 
+*/
+            public static void main(String[] args) throws Exception {
+
+               int numero1 = 1;
+               String numero2 = "2";
+               System.out.println(numero1+numero2); 
+             }
+         
 }
-}
+
