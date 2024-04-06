5d1fcd691bc569362a5f41fce7f6f3b3e8aa797d
Daiane Cruz <daianercruz12@gmail.com>
2024-04-06 13:51:43 -0300

    Controle de fluxo feito apenas com IF

---
+++ learning-java/src/ControleFluxo/ControleFluxo.java
@@ -0,0 +1,21 @@
+package ControleFluxo;
+
+public class ControleFluxo {
+
+
+    public static void main(String[] args) {
+        
+            double saldo = 25.0;
+            double valorSolicitado = 4.0;
+ 
+                if(valorSolicitado < saldo)
+                    saldo = saldo - valorSolicitado;
+ 
+                 System.out.println(saldo);
+ 
+
+
+
+    }
+    
+}
