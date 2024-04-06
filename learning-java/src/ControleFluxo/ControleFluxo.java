719a7b48423e79305733fbf16fba06e0f1992d87
Daiane Cruz <daianercruz12@gmail.com>
2024-04-06 14:04:06 -0300

    Adicionando Else para apresentar o saldo insuficiente

--- learning-java/src/ControleFluxo/ControleFluxo.java
+++ learning-java/src/ControleFluxo/ControleFluxo.java
@@ -6,12 +6,16 @@ public class ControleFluxo {
     public static void main(String[] args) {
         
             double saldo = 25.0;
-            double valorSolicitado = 4.0;
+            double valorSolicitado = 50.0;
  
-                if(valorSolicitado < saldo)
+                if(valorSolicitado < saldo){
                     saldo = saldo - valorSolicitado;
- 
-                 System.out.println(saldo);
+                    System.out.println(saldo);
+                }
+                else {
+                    System.out.println("Valor insuficiente: " + valorSolicitado);
+                }
+                 
  
 
 
