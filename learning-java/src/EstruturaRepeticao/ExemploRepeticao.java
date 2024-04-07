51f83ef81c2640962fa57f9f95a056e9767ea7ea
Daiane Cruz <daianercruz12@gmail.com>
2024-04-07 10:29:51 -0300

    Estrutura de Repetição: For

---
+++ learning-java/src/EstruturaRepeticao/ExemploRepeticao.java
@@ -0,0 +1,16 @@
+package EstruturaRepeticao;
+
+public class ExemploRepeticao {
+
+    public static void main(String[] args) {
+
+            for (int carneirinhos = 0; carneirinhos <=20; carneirinhos ++ ){
+                System.out.println("Contando Carneirinhos: " + carneirinhos);
+        }
+
+        System.out.println("Maria dormiu");
+
+        
+    }
+    
+}
